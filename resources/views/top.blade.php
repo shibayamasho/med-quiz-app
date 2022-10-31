f1682759c5d6c6a13eabb32b0c753ecbd12c9b11
shibayamasho <men.in.boss2@gmail.com>
2022-10-31 15:27:51 +0900

    topページ

--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -5,9 +5,39 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>med-quiz</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body>
-    <h1>TOP</h1>
-    <p>this is top page.</p>
+    <header class="h-24 bg-green-400 flex justify-between">
+        <div class="bg-blue-100 p-3 flex items-center">LOGO</div>
+        <ul class="flex">
+            <li class="bg-blue-100 m-1 p-3 flex items-center"><a href="">menu1</a></li>
+            <li class="bg-blue-100 m-1 p-3 flex items-center"><a href="">menu2</a></li>
+            <li class="bg-blue-100 m-1 p-3 flex items-center"><a href="">menu3</a></li>
+            <li class="bg-blue-100 m-1 p-3 flex items-center">user name<?php // ユーザー名 ?></li>
+        </ul>
+    </header>
+
+    <main class="w-8/12 bg-blue-200 space-x-4">
+
+        <h1 class="text-3xl ml-4">TOP</h1>
+        <p>this is top page.</p>
+
+        <section class="">
+            <div><a href="" class="inline-block bg-green-600 text-white p-2 rounded-lg">新しいカテゴリーを登録</a></div>
+        </section>
+
+        <section class="">
+            <h2 class="text-2xl">カテゴリー 一覧</h2>
+
+            <div>
+                <h3>HTML<?php // カテゴリー名 ?></h3>
+                <a href="#">問題ページへ</a>
+            </div>
+
+        </section>
+
+    </main>
+
 </body>
 </html>
